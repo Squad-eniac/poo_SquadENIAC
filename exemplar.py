39a0a6996f62446d0642df58d62168bf6852bb81
Kassia ES <kassiaes@gmail.com>
2024-08-12 14:07:11 -0300

    ajuste na visibilidade dos atributos

--- exemplar.py
+++ exemplar.py
@@ -1,23 +1,23 @@
 class Exemplar:
     def __init__(self, id):
-        self.id = id
-        self.disponivel = True
-        self.numAtualRenovacoes = 0
+        self.__id = id
+        self.__disponivel = True
+        self.__numAtualRenovacoes = 0
 
     def emprestar(self):
-        if self.disponivel:
-            self.disponivel = False
-            print(f"Exemplar {self.id} - empréstimo feito com sucesso")
+        if self.__disponivel:
+            self.__disponivel = False
+            print(f"Exemplar {self.__id} - empréstimo feito com sucesso")
         else:
-            print(f"Exemplar {self.id} - não está disponível para empréstimo")
+            print(f"Exemplar {self.__id} - não está disponível para empréstimo")
 
     def devolver(self):
-        if not self.disponivel:
-            self.disponivel = True
-            print(f"Exemplar {self.id} - devolvido com sucesso")
+        if not self.__disponivel:
+            self.__disponivel = True
+            print(f"Exemplar {self.__id} - devolvido com sucesso")
         else:
-            print(f"Exemplar {self.id} - não está com empréstimo ativo")
+            print(f"Exemplar {self.__id} - não está com empréstimo ativo")
     
     def renovar(self):
-        self.numAtualRenovacoes += 1
-        print(f"Exemplar {self.id} foi renovado. Renovação atual: {self.numAtualRenovacoes}.")
+        self.__numAtualRenovacoes += 1
+        print(f"Exemplar {self.__id} foi renovado. Renovação atual: {self.__numAtualRenovacoes}.")
